befa0aaea6bdd706cfd7ad5bd10e652a26b18947
evoxalien <crazzy.john@gmail.com>
2014-01-23 18:12:03 -0600

    Entity Manager

---
+++ WindowsGame2/WindowsGame2/EntityManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace GeometryWars
+{
+    static class EntityManager
+    {
+        static List<Entity> entities = new List<Entity>();
+
+        static bool isUpdating;
+        static List<Entity> addedEntities = new List<Entity>();
+
+        public static void Add(Entity entity)
+        {
+            if (!isUpdating)
+                entities.Add(entity);
+            else
+                addedEntities.Add(entity);
+        }
+
+        public static void Update()
+        {
+            isUpdating = true;
+
+            foreach (var entity in entities)
+                entity.Update();
+
+            isUpdating = false;
+
+            foreach (var entity in addedEntities)
+                entities.Add(entity);
+
+            addedEntities.Clear();
+
+            // remove any expired entities
+            entities = entities.Where(x => !x.IsExpired).ToList();
+
+        }
+    }
+}
